b345fe1e49452bee72d04efe1789ca414eab16cd
davschne <davschne@gmail.com>
2015-08-19 10:23:55 -0700

    Added featured content links

--- src/app.jsx
+++ src/app.jsx
@@ -34,14 +34,96 @@ var HeaderContainer = React.createClass({
   }
 });
 
+var Article = React.createClass({
+  render: function() {
+    return (
+      <a href={this.props.contents.link}>
+        <article>
+          <h2>{this.props.contents.title}</h2>
+          <ul>
+            <li>By {this.props.contents.author}</li>
+            <li>{this.props.contents.date}</li>
+            <li>{this.props.contents.views} Views</li>
+            <li>{this.props.contents.numComments} Comments</li>
+          </ul>
+        </article>
+      </a>
+    );
+  }
+});
+
+var RelatedContainer = React.createClass({
+  render: function() {
+    var relatedArticles = this.props.contents.map(function(article) {
+      return (
+        <Article contents={article}/>
+      );
+    });
+    return (
+      <div id="related">
+        {relatedArticles}
+      </div>
+    );
+  }
+});
+
+var FeaturedContainer = React.createClass({
+  getInitialState: function() {
+    return ({
+      article: {
+        title: "10 Reasons You're an Idiot Just Like Everybody Else",
+        author: "John Cheese",
+        date: "August 32, 2012",
+        views: 156835,
+        numComments: 200,
+        link: "http://www.crack'd.com/youre-an-idiot.html"
+      },
+      related: [
+        {
+          title: "Why That Movie Everybody Likes Is Actually Stupid",
+          author: "Skip Spoiler",
+          date: "August 28, 2012",
+          views: 4024,
+          numComments: 321,
+          link: "http://www.crack'd.com/stupid-movie.html"
+        },
+        {
+          title: "5 Popular Video Games That Are Absurd, Xenophobic Bullshit",
+          author: "J.F. Sargent",
+          date: "August 29, 2012",
+          views: 3216,
+          numComments: 379,
+          link: "http://www.crack'd.com/absurd-bullshit-games.html"
+        },
+        {
+          title: "8 Movie Themes From The 80s That Probably Warped You For Life",
+          author: "A.C. Grimes",
+          date: "August 31, 2012",
+          views: 46223,
+          numComments: 421,
+          link: "http://www.crack'd.com/movies-that-warped-you.html"
+        }
+      ]
+    });
+  },
+  render: function() {
+    return (
+      <main>
+        <Article contents={this.state.article}/>
+        <aside>
+          <RelatedContainer contents={this.state.related}/>
+        </aside>
+      </main>
+    );
+  }
+});
+
 var App = React.createClass({
   render: function() {
     return (
       <body>
         <HeaderContainer/>
-        <main>
-
-        </main>
+        <FeaturedContainer/>
       </body>
     );
   }
